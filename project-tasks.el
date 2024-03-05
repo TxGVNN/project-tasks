6a97747d53ba1e0c2cb7cfc7f4e9e3011e8e4208
Giap Tran <txgvnn@disroot.org>
2024-03-05 16:28:50 +0700

    feat: Add project-tasks-files and project-tasks-ignore-files
    BREAKING:
    - Remove `project-tasks-file' var
    - Remove capture and jump features

--- project-tasks.el
+++ project-tasks.el
@@ -5,8 +5,8 @@
 ;; Author: Giap Tran <txgvnn@gmail.com>
 ;; Keywords: project, workflow, tools
 ;; Homepage: https://github.com/TxGVNN/project-tasks
-;; Package-Requires: ((emacs "26.1"))
-;; Version: 0.4.1
+;; Package-Requires: ((emacs "26.1") (project "0.6.0"))
+;; Version: 0.5.0
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -22,14 +22,14 @@
 ;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
-;;`project-tasks` provides efficient task management for your project.
+;; `project-tasks' provides efficient task management for your project.
 ;; It allows you to organize and manage your project's tasks in an
-;; Org file. You can quickly jump to the task file, capture new tasks
+;; Org file.  You can quickly jump to the task file, capture new tasks
 ;; and easily run tasks by name.
 ;;
-;; The main idea is to use Org source blocks as tasks. Each source
-;; block is a task and its name is the task name. You can use
-;; `project-tasks` to quickly run a task by name.
+;; The main idea is to use Org source blocks as tasks.  Each source
+;; block is a task and its name is the task name.  You can use
+;; `project-tasks' to quickly run a task by name.
 ;;
 ;; Example:
 ;; If we want to have a fast way to run `top` command in our project,
@@ -39,7 +39,7 @@
 ;; (comint-run "top")
 ;; #+end_src
 ;;
-;; Then we just need to run `M-x project-tasks` to select and run the task.
+;; Then we just need to run M-x project-tasks to select and run the task.
 
 ;;; Code:
 (require 'org)
@@ -49,8 +49,18 @@
   "Project tasks."
   :group 'project)
 
-(defcustom project-tasks-file "tasks.org"
-  "File to store project tasks."
+(defcustom project-tasks-files '("tasks.org")
+  "List of task files, the file can be a regexp pattern."
+  :type '(repeat string)
+  :group 'project-tasks)
+
+(defcustom project-tasks-ignore-files nil
+  "List of ignored task files."
+  :type '(repeat string)
+  :group 'project-tasks)
+
+(defcustom project-tasks-separator " -> "
+  "Separator to separate file name and task name."
   :type 'string
   :group 'project-tasks)
 
@@ -76,45 +86,46 @@
   (let ((src-block-names (org-babel-src-block-names)))
     (unless src-block-names
       (error "No source blocks found in current buffer"))
-    (let ((task (completing-read "Task: " src-block-names nil t)))
+    (let ((task (completing-read "Select task: " src-block-names nil t)))
       (project-tasks--eval task))))
 
+(defun project-tasks--get-task-files ()
+  "Get list of task files and exclude ignored files."
+  (let* ((project-root-dir (funcall project-tasks-root-func))
+         (project-files (mapcar (lambda (file)
+                                  (file-relative-name file project-root-dir))
+                                (project-files (project-current t))))
+         (files (cl-remove-if (lambda (file)
+                                (or (cl-some (lambda (pattern)
+                                               (string-match-p pattern file))
+                                             project-tasks-ignore-files)
+                                    (not (cl-some (lambda (pattern)
+                                                    (string-match-p pattern file))
+                                                  project-tasks-files))))
+                              project-files)))
+    files))
 
 ;;;###autoload
-(defun project-tasks (file)
-  "Open project tasks FILE and display tasks."
-  (interactive "P")
-  (setq file (if (equal file '(4))
-                 (read-file-name "Choose file: ")
-               file))
-  (let* ((org-default-notes-file
-          (or file
-              (concat (funcall project-tasks-root-func) project-tasks-file))))
-    (with-current-buffer (find-file-noselect org-default-notes-file)
-      (project-tasks-current-buffer))))
-
-;;;###autoload
-(defun project-tasks-in-dir (dir)
-  "Run `project-tasks' in DIR."
-  (interactive "D")
-  (project-tasks (concat dir project-tasks-file)))
-
-;;;###autoload
-(defun project-tasks-capture ()
-  "Org capture to project."
+(defun project-tasks ()
+  "Open project tasks and display tasks."
   (interactive)
-  (unless (featurep 'org-capture) (require 'org-capture))
-  (let* ((project (funcall project-tasks-root-func))
-         (org-default-notes-file (concat project project-tasks-file)))
-    (call-interactively 'org-capture)))
-
-;;;###autoload
-(defun project-tasks-jump ()
-  "Jump to the project task file."
-  (interactive)
-  (let* ((project (funcall project-tasks-root-func))
-         (org-default-notes-file (concat project project-tasks-file)))
-    (find-file org-default-notes-file)))
+  (let* ((project-root-dir (funcall project-tasks-root-func))
+         (src-block-names (mapcar (lambda (file)
+                                    (let ((file-in-project (file-relative-name file project-root-dir)))
+                                      (mapcar (lambda (x)
+                                                (concat file-in-project project-tasks-separator x))
+                                              (with-current-buffer (find-file-noselect file)
+                                                (org-babel-src-block-names)))))
+                                  (project-tasks--get-task-files))))
+    (unless src-block-names
+      (error "No source blocks found in project"))
+    (let ((task (completing-read "Select task: " (apply #'append src-block-names) nil t)))
+      (let* ((task-split (split-string task project-tasks-separator))
+             (org-default-notes-file (car task-split))
+             ;; task-name is rest of the string after the first project-tasks-separator
+             (task-name (mapconcat #'identity (cdr task-split) project-tasks-separator)))
+        (with-current-buffer (find-file-noselect org-default-notes-file)
+          (project-tasks--eval task-name))))))
 
 (provide 'project-tasks)
 ;;; project-tasks.el ends here
